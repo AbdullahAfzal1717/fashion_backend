7264e5ff9c3ed51fa6cbd4c26736d156ea7c3d38
Abdullah Afzal <abdullah941685@gmail.com>
2025-12-18 09:37:44 +0500

    b

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
 import imageRoutes from "./routes/imageRoutes.js";
+import dotenv from "dotenv";
+
 
 const app = express();
-app.use(express.json());
 
 
-
+dotenv.config()
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API is running");
@@ -15,3 +17,15 @@ app.get("/", (req, res) => {
 
 app.use("/api", imageRoutes);
 
+const startServer = () => {
+    try {
+        // connectDB(process.env.MONGODB_URL)
+        app.listen(5000, () => {
+            console.log("Server listening on 5000 http://localhost:5000");
+        });
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+startServer();
