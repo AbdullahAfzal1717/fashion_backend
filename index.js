8d7f1b1076ae8aec073e02e3f9bf975b3afd0aa3
Abdullah Afzal <abdullah941685@gmail.com>
2025-12-18 09:32:36 +0500

    a

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import imageRoutes from "./routes/imageRoutes.js";
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 
